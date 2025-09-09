9bcc87f95ddfec11c3cfaa467f053d8d88b7ffc9
navya01106 <navyaagarwal01106@gmail.com>
2025-09-09 23:54:40 +0530

    day4

---
+++ day3/q5.c
@@ -0,0 +1,13 @@
+//Write a program to convert temperature from Celsius to Fahrenheit.
+# include<stdio.h>
+int main()
+{
+float c,f;
+printf("enter temperature in celcius");
+scanf(" %f", &c);
+f=(c*1.8)+32;
+printf("the temperature in fahrenheit is %f" ,f);
+
+return 0;
+
+}
