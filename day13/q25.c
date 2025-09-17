09dc54b1e50cb2591fb354e87d75d19f4cb5e510
navya01106 <navyaagarwal01106@gmail.com>
2025-09-17 22:09:57 +0530

    day13

---
+++ day13/q25.c
@@ -0,0 +1,34 @@
+/*Write a program to implement a basic calculator using switch-case for +, -, *, /, %.*/
+#include <stdio.h>
+int main()
+{
+    int a,b,result;
+    char op;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c",&op);
+    switch(op)
+    {
+        case '+':
+        result=a+b;
+        break;
+        case '-':
+        result=a-b;
+        break;
+        case '*':
+        result=a*b;
+        break;
+        case '/':
+        result=a/b;
+        break;
+        case '%':
+        result=a%b;
+        break;
+        default:
+        printf("Invalid operator");
+        
+    }
+    printf("Result: %d",result);
+    return 0;
+}
