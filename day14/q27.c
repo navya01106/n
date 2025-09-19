7d2469720fb779b712a9fb29a57895a14d13b7e2
navya01106 <navyaagarwal01106@gmail.com>
2025-09-20 01:02:14 +0530

    day14

--- day14/q27.c
+++ day14/q27.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+//Write a program to print the sum of the first n odd numbers.
+#include<stdio.h>
 int main()
 {
     int n,i,sum=0;
--- day14/q28.c
+++ day14/q28.c
@@ -1,10 +1,12 @@
+//Write a program to print the product of even numbers from 1 to n.
 #include<stdio.h>
 int main()
 {
-    int n,sum=0;
+    int n,i,product=1;
     printf("enter any number");
     scanf("%d",&n);
-    sum=2*n;
-    printf("sum=%d",sum);
+    for(i=2;i<=n;i=i+2)
+    product=product*i;
+    printf("product=%d",product);
     return 0;
 }
